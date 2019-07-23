3f916a8fe7d292ecf3114193da64ebb6e3af1b54
Houssem Dellai <houssem.dellai@live.com>
2019-07-23 16:30:15 +0200

    Added data validation using FluentValidation nuget package

---
+++ ProductsApp/ProductsApp/Validators/ProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace ProductsApp.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(4);
+        }
+    }
+}
--- ProductsApp/ProductsApp/ViewModels/DetailsViewModel.cs
+++ ProductsApp/ProductsApp/ViewModels/DetailsViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using FluentValidation.Results;
+using ProductsApp.Validators;
+using Xamarin.Forms;
 
 namespace ProductsApp.ViewModels
 {
     public class DetailsViewModel : INotifyPropertyChanged
     {
         private Product _product;
+        private string _error;
 
         public Product Product
         {
@@ -15,6 +20,27 @@ namespace ProductsApp.ViewModels
                 _product = value;
                 OnPropertyChanged();
             }
+        }
+
+        public string Error
+        {
+            get => _error;
+            set
+            {
+                _error = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ValidateCommand => new Command(Validate);
+
+        public void Validate()
+        {
+            var validator = new ProductValidator();
+
+            var result = validator.Validate(Product);
+
+            Error = result.IsValid ? string.Empty : "Data is not valid.";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
